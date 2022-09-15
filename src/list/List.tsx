ba6c69e71d00ee4c1cae9531fd44e6f57561cd00
allen <eepson123@gmail.com>
2022-09-15 23:43:46 +0800

    feat:Add drag drop

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 interface User {
   userId: String
   username: String
@@ -8,11 +9,46 @@ interface User {
   registeredAt: any
 }
 
-function List({ list }: { list: User[] }) {
+function List({ list, children }: { list: User[]; children?: JSX.Element }) {
+  const [x, setList] = useState(list)
+  const dragItem: React.MutableRefObject<any> = useRef(null)
+  const dragOverItem: React.MutableRefObject<any> = useRef(null)
+
+  const dragStart = (e: React.DragEvent, position: number) => {
+    // This will get executed only for nullRef and not for the other two refs
+    dragItem.current = position
+    const element = e.target as HTMLElement
+    if (element.tagName !== 'FIGURE') return
+    element.classList.add('bg-red-400')
+  }
+
+  const dragEnter = (e: React.DragEvent, position: number) => {
+    dragOverItem.current = position
+    const element = e.target as HTMLElement
+    if (element.tagName !== 'FIGURE') return
+    console.log(element.tagName)
+  }
+  const enableDropping = (event: React.DragEvent) => {
+    event.preventDefault()
+  }
+  const drop = (e: React.DragEvent) => {
+    const copyListItems = [...x]
+    const dragItemContent = copyListItems[dragItem.current]
+    copyListItems.splice(dragItem.current, 1)
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent)
+    dragItem.current = null
+    dragOverItem.current = null
+    setList(copyListItems)
+  }
   return (
     <div className='flex flex-wrap justify-around  text-center'>
-      {list.map(({ avatar, username, userId, email }, i) => (
+      {x.map(({ avatar, username, userId, email }, i) => (
         <figure
+          onDragStart={(e) => dragStart(e, i)}
+          onDragEnter={(e) => dragEnter(e, i)}
+          onDragOver={enableDropping}
+          onDrop={drop}
+          draggable
           key={`${userId}`}
           className='bg-slate-100 rounded-xl p-8 dark:bg-slate-800 w-[500px] m-1 last:mr-auto'
         >
