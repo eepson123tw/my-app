3a4ab5024fb138722d4a6f78941b81d1a5732ad9
allen <eepson123@gmail.com>
2022-09-26 00:19:30 +0800

    feat:Add the useEffect hook~

--- src/routes/hooks.tsx
+++ src/routes/hooks.tsx
@@ -1,31 +1,53 @@
-import { useState, useReducer } from 'react'
+import { useState, useReducer, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Hooks() {
-  const [inputValue, setInputValue] = useState('app')
+  // const [inputValue, setInputValue] = useState('app')
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-  }
+  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setInputValue(e.target.value)
+  // }
 
-  const reducer = (
-    state: { count: number; showText: boolean },
-    action: { type: string }
-  ): any => {
-    switch (action.type) {
-      case 'INCREMENT':
-        return { count: state.count + 1, showText: state.showText }
-      case 'toggleShow':
-        return { count: state.count, showText: !state.showText }
-      default:
-        return state
-    }
-  }
+  // const reducer = (
+  //   state: { count: number; showText: boolean },
+  //   action: { type: string }
+  // ): any => {
+  //   switch (action.type) {
+  //     case 'INCREMENT':
+  //       return { count: state.count + 1, showText: state.showText }
+  //     case 'toggleShow':
+  //       return { count: state.count, showText: !state.showText }
+  //     default:
+  //       return state
+  //   }
+  // }
 
-  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true })
+  const [fakeData, setFakeData] = useState<[{ name: string; id: number }]>([
+    { name: '', id: 0 }
+  ])
+
+  // const [state, dispatch] = useReducer(reducer, { count: 0, showText: true })
+
+  useEffect(() => {
+    // when render execute useEffect fn will run,include changing the state ,because changing state will rerender the Page(*)
+    // when page render useEffect will render once and change the state will can once (render twice)
+    // use empty ary to call once , or pass the watching state which you want to focus at
+    axios.get('https://jsonplaceholder.typicode.com/comments').then((res) => {
+      setFakeData(res.data)
+      // console.log(123)
+    })
+  }, [])
 
   return (
     <div className='App mt-5 mx-auto  container   bg-sky-500 '>
-      <input
+      {fakeData &&
+        fakeData.map((d) => (
+          <h2 key={d.id}>
+            {d.id} `&gt;`
+            {d.name}
+          </h2>
+        ))}
+      {/* <input
         type='text'
         placeholder='insert'
         className='border'
@@ -43,7 +65,7 @@ export default function Hooks() {
         }}
       >
         123
-      </button>
+      </button> */}
     </div>
   )
 }
