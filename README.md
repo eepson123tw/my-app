fcae71e2deb9b566f65614a0531a0ca6a57514d6
allen <eepson123@gmail.com>
2022-09-29 00:13:58 +0800

    feat:Add the useMemo and useCallback

--- README.md
+++ README.md
@@ -7,19 +7,20 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 1.**useState** : is about to store data useing the js clourse and return the changing data. 
 
-2.**useEffect** : every render will evoke this api ， and user can choose clean Effect or not, also can ignore the change ，only some condition be complished.
-
-3.**useRef**: like an domQuerySelector help developer use dom element easily
+2.**useRef**: like an domQuerySelector help developer use dom element easily
               ex.get domValue or get element focus use with few code
 
-4.**useEffect**:  when render execute useEffect fn will run,include changing the state ,because changing state will rerender the Pag(*)。
+3.**useEffect**:  when render execute useEffect fn will run,include changing the state ,because changing state will rerender the Pag(*)。
                   when page render useEffect will render once and change the state will can once (render twice)
                   use empty ary to call once , or pass the watching state which you want to focus at
 
-5.**useReducer**: like useState but we can set the  situation  which we want to change multiple states
+4.**useReducer**: like useState but we can set the  situation  which we want to change multiple states
 
-6.**useLayoutEffect**:before the useEffect, before print the page, can use at some ui change UseCase
+5.**useLayoutEffect**:before the useEffect, before print the page, can use at some ui change UseCase
 
-7.**useImperativeHandle**:can call some function outside and made component more useable. like vue ref and pass props to do someThing.
+6.**useImperativeHandle**:can call some function outside and made component more useable. like vue ref and pass props to do someThing.
 
-8.**useContext**:To create State which can pass multiple component and like an global controller.(createContext<-need to pass value(which we want to control),useContext)
+7.**useContext**:To create State which can pass multiple component and like an global controller.(createContext<-need to pass value(which we want to control),useContext)
+8.**useMemo**:To improve performance and be able to decrease latency on huge computations like the vue computed(only dependency change the hook will invoke )
+9.**useCallback**:like the useMemo but no need to return value,and to control the function reCreate by state changing.
+
--- src/routes/hooks.tsx
+++ src/routes/hooks.tsx
@@ -4,7 +4,8 @@ import {
   useReducer,
   useEffect,
   useLayoutEffect,
-  createContext
+  createContext,
+  useMemo
 } from 'react'
 import axios from 'axios'
 
@@ -16,13 +17,16 @@ export const AppContext = createContext<{
 } | null>(null)
 export default function Hooks() {
   //before the useEffect, before print the page
-  useLayoutEffect(() => {
-    console.log('useLayoutEffect')
-  }, [])
+  // useLayoutEffect(() => {
+  //   console.log('useLayoutEffect')
+  // }, [])
   // only called after render page => vary fast and user can not see
-  useEffect(() => {
-    console.log('useEffect')
-  }, [])
+  // useEffect(() => {
+  //   console.log('useEffect')
+  // }, [])
+
+  // const getLongestName = useMemo(() => findName(data), [data]) //when Data has change ~
+
   // const [inputValue, setInputValue] = useState('app')
 
   // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,9 +47,11 @@ export default function Hooks() {
   //   }
   // }
 
-  // const [fakeData, setFakeData] = useState<[{ name: string; id: number }]>([
-  //   { name: '', id: 0 }
-  // ])
+  const [fakeData, setFakeData] = useState<[{ name: string; id: number }]>([
+    { name: '', id: 0 }
+  ])
+
+  const [toggle, setToggle] = useState(false)
 
   //useRef like an domQuerySelector help developer use dom element easily
   // const inputRef = useRef<HTMLInputElement>(null)
@@ -58,20 +64,27 @@ export default function Hooks() {
 
   // const [state, dispatch] = useReducer(reducer, { count: 0, showText: true })
 
-  // useEffect(() => {
-  //   axios.get('https://jsonplaceholder.typicode.com/comments').then((res) => {
-  //     setFakeData(res.data)
-  //   })
-  // }, [])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/comments').then((res) => {
+      setFakeData(res.data)
+    })
+  }, [])
+
+  const counter = (ary: [{ name: string; id: Number }]) => {
+    console.log(123)
+    return '123'
+  }
+
+  const memoCounter = useMemo(() => counter(fakeData), [fakeData])
 
   const [userName, setUserName] = useState('')
 
   return (
     <div className='App mt-5 mx-auto  container   bg-sky-500 '>
-      <AppContext.Provider value={{ userName, setUserName }}>
+      {/* <AppContext.Provider value={{ userName, setUserName }}>
         <User></User>
         <Login></Login>
-      </AppContext.Provider>
+      </AppContext.Provider> */}
 
       {/* {fakeData &&
         fakeData.map(
@@ -82,9 +95,19 @@ export default function Hooks() {
                 {d.name}
               </h2>
             )
-        )}
+        )} */}
 
-      <div className='flex'>
+      <p>{memoCounter}</p>
+
+      {toggle && <h2>toggle</h2>}
+      <button
+        className='border border-red-300 text-black'
+        onClick={() => setToggle(!toggle)}
+      >
+        add click
+      </button>
+
+      {/* <div className='flex'>
         <input
           type='text'
           name='app'
