9ab11f125b25a07357d131d6e8ffada4fc455367
allen <eepson123@gmail.com>
2022-10-20 00:56:05 +0800

    feat:Add the todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+interface todo {
+  name: string
+}
 
 export default function TodoList() {
-  const [name, setName] = useState('')
+  // const [name, setName] = useState('')
+  const [todo, setTodo] = useState<todo[]>([])
+  const todoRef = useRef<HTMLInputElement | null>(null)
 
-  const inputHandler = (e: React.FormEvent<EventTarget>): void => {
-    let ans = e.target as HTMLInputElement
-    setName(ans.value)
+  // const inputHandler = (e: React.FormEvent<EventTarget>): void => {
+  //   let ans = e.target as HTMLInputElement
+  //   setTodo((x) => [...x, { name: ans.value }])
+  // }
+  const clickHandler = (): void => {
+    let val = todoRef
+    if (val.current?.value && val.current?.value !== '') {
+      setTodo((x) => [...x, { name: `${val?.current?.value}` }])
+      setTimeout(() => {
+        val.current && (val.current.value = '')
+      }, 1)
+    }
   }
 
   useEffect(() => {
-    console.log('useEffect')
+    // console.log('useEffect')
   }, [])
 
   return (
@@ -18,9 +33,15 @@ export default function TodoList() {
         <label htmlFor='name' className='pr-5'>
           Input Your Name:
         </label>
-        <input type='text' id='name' onInput={(e) => inputHandler(e)} />
+        <input type='text' id='name' ref={todoRef} />
+        <button
+          className='btn bg-neutral-400 border border-r-amber-200'
+          onClick={clickHandler}
+        >
+          Click
+        </button>
       </div>
-      <p>input:{name}</p>
+      {todo.length && todo.map((x) => `<p>${x.name}</p>`)}
     </div>
   )
 }
